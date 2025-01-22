4ce59125650a1afb02d7c37c27f3a7cac92d05e7
kwong44 <kylenwong48@gmail.com>
2025-01-22 11:14:13 +0700

    updated landing

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -16,7 +16,7 @@ const Page: FC = () => {
             City Treasure Hunt
           </h1>
           <p className="text-xl text-amber-700 max-w-2xl mx-auto">
-            Explore the city, solve clues, complete side quests and win prizes in this exciting hunt!
+            Complete Side Quests & Win Prizes in this free Treasure Hunt!
           </p>
         </section>
 
@@ -27,15 +27,13 @@ const Page: FC = () => {
             </h2>
             <div className="prose">
               <ol className="list-decimal list-inside space-y-4">
-                <li>Message us on <a href="https://wa.me/message/OSXOZZSAAUXCP1" className="text-amber-700 hover:underline">WhatsApp</a> or <a href="https://www.instagram.com/alohavietnamretreat/profilecard/?igsh=M2hwaWFoODF2ZnM0" className="text-amber-700 hover:underline">Instagram</a> to join the hunt as an individual or team</li>
-                <li>Join the <a href="https://chat.whatsapp.com/Gzff8wsNHlqERDmtD8Bck4" className="text-amber-700 hover:underline">WhatsApp Group</a> to find a team, get updates, share hints (or not lol) and more</li>
-                <li>Once we receive your message, we will send you the first clue or you can check the <a href="/clues" className="text-amber-700 hover:underline">Clues</a> page</li>
+                <li>Join the <a href="https://chat.whatsapp.com/Gzff8wsNHlqERDmtD8Bck4" className="text-amber-700 hover:underline">WhatsApp Group</a> to join the hunt!</li>
+                <li>Check the <a href="/clues" className="text-amber-700 hover:underline">Clues</a> page to see the first clue</li>
                 <li>Visit the <a href="/dashboard" className="text-amber-700 hover:underline">Dashboard</a> to see the map and Teams Leaderboard</li>
-                <li>Solve clues to unlock new locations</li>
-                <li>Visit locations to find verification codes</li>
-                <li>Enter codes to unlock more clues</li>
+                <li>Solve clues to find cool hidden-gem businesses in the city. Each business has a verification code that you can enter to unlock the next clue</li>
                 <li>Share your adventure on Instagram with #AVNTreasureHunt</li>
-                <li>Complete <a href="/challenges" className="text-amber-700 hover:underline">Side Quests</a> and post them to your story and tag us for additional points</li>
+                <li>Complete <a href="/challenges" className="text-amber-700 hover:underline">Side Quests</a> and post them for additional points</li>
+                <li>The top teams will win small prizes such as vouchers for surf lessons, yoga classes, and more</li>
               </ol>
             </div>
           </section>
