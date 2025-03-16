95cf97d6d9c8ee4276eb67f710b6b92015883992
ttd11204 <datkk112004@gmail.com>
2025-03-16 12:06:36 +0700

    gallery

---
+++ types/gallery.types.ts
@@ -0,0 +1,15 @@
+export interface GalleryImage {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  caption?: string;
+  team_name?: string;
+  location?: string;
+  created_at: string;
+}
+
+export interface GalleryResponse {
+  images: GalleryImage[];
+  error?: string;
+}
