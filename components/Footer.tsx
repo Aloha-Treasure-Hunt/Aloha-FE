45f74cb0d9530b4f54913b8d99d5ef8e6de7b899
ttd11204 <datkk112004@gmail.com>
2025-03-22 00:09:07 +0700

    css login and homepgae

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,22 +9,12 @@ import '@/components/home/home.css';
 
 export function Footer() {
   return (
-    <footer className='footer-bg p-4 grid grid-cols-5 gap-2 items-center justify-items-center fixed bottom-0 w-full z-20'>
-      <div className='text-center'>
-        <FaCompass className='text-red-800 text-xl' />
-      </div>
-      <div className='text-center'>
-        <FaQuestionCircle className='text-red-800 text-xl' />
-      </div>
-      <div className='text-center'>
-        <FaBell className='text-red-800 text-xl' />
-      </div>
-      <div className='text-center'>
-        <FaLanguage className='text-red-800 text-xl' />
-      </div>
-      <div className='text-center'>
-        <FaUserCircle className='text-red-800 text-xl' />
-      </div>
+    <footer className='footer-bg'>
+      <FaCompass />
+      <FaQuestionCircle />
+      <FaBell />
+      <FaLanguage />
+      <FaUserCircle />
     </footer>
   );
 }
