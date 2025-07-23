001b7462572a49c466bbc5cb270d952389480cc8
ttd11204 <ttd11204@gmail.com>
2025-07-23 11:50:27 +0700

    Update base URL for API in customizeAxios.js

--- components/api/customizeAxios.js
+++ components/api/customizeAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://aloha-vietnam.azurewebsites.net/api/",
+  baseURL: "https://aloha-h7ahbsgdhxedatax.southeastasia-01.azurewebsites.net/api/",
 });
 
 // Add a response interceptor
